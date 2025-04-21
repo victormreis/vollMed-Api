7539d2b90f0a601778be074f1fdc3905a92a441f
Victor Martins <vitor_jhoul@hotmail.com>
2025-04-21 17:42:42 -0400

    Fix: Including Doctor id into json list returned to the frontend

--- src/main/java/med/vol/api/dto/DoctorListDTO.java
+++ src/main/java/med/vol/api/dto/DoctorListDTO.java
@@ -3,8 +3,8 @@ package med.vol.api.dto;
 import med.vol.api.model.Doctor;
 import med.vol.api.types.Specialty;
 
-public record DoctorListDTO(String name, String email, String mra, Specialty specialty) {
+public record DoctorListDTO(Long id,String name, String email, String mra, Specialty specialty) {
     public DoctorListDTO(Doctor doctor) {
-        this(doctor.getName(), doctor.getEmail(), doctor.getMra(), doctor.getSpecialty());
+        this(doctor.getId(),doctor.getName(), doctor.getEmail(), doctor.getMra(), doctor.getSpecialty());
     }
 }
