714bcf76e75fe115f46d951a0761719aca9bd609
Victor Martins <vitor_jhoul@hotmail.com>
2025-04-24 15:14:36 -0400

    Feat: Error Handling

---
+++ src/main/java/med/vol/api/errorHandling/ErrorHandling.java
@@ -0,0 +1,41 @@
+package med.vol.api.errorHandling;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class ErrorHandling {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity notFoundError() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity invalidFields(MethodArgumentNotValidException err){
+        var errors = err.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(FieldsErrorValidation::new).toList());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity duplicatedData(SQLIntegrityConstraintViolationException err){
+
+        return ResponseEntity.badRequest().body(err.getLocalizedMessage());
+    }
+
+
+    private record FieldsErrorValidation(String field, String message) {
+        public FieldsErrorValidation(FieldError err) {
+            this(err.getField(), err.getDefaultMessage());
+        }
+    }
+
+
+
+}
