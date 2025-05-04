e02978c1e56e05adc28bacaa6dbc788c698ae3fc
Victor Martins <vitor_jhoul@hotmail.com>
2025-05-03 23:38:20 -0400

    Feat: Work Hours validation

--- src/main/java/med/vol/api/validation/WorkHoursValidation.java
+++ src/main/java/med/vol/api/validation/WorkHoursValidation.java
@@ -2,18 +2,20 @@ package med.vol.api.validation;
 
 import jakarta.validation.ValidationException;
 import med.vol.api.dto.BookAppointmentsDTO;
+import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
 
-public class WorkHoursValidation {
+@Component
+public class WorkHoursValidation implements BookAppointmentValidator {
 
-    public void validateWorkHours(BookAppointmentsDTO data) {
-        var bookTime = data.date().getHour();
+    public void validate(BookAppointmentsDTO data) {
+        var bookTime = data.dateAndTime().getHour();
 
         var openHour = bookTime < 7;
         var closeHour = bookTime > 18;
 
-        var sunday = data.date().getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var sunday = data.dateAndTime().getDayOfWeek().equals(DayOfWeek.SUNDAY);
 
         if(openHour || closeHour || sunday) {
             throw new ValidationException("Time selected must be between work hours: mon - sat 7AM - 7PM");
