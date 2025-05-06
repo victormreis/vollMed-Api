0a8964523fb379c4c2854bf27252a54d95f92ade
Victor Martins <vitor_jhoul@hotmail.com>
2025-05-06 16:18:21 -0400

    test: Configure test environment and database

---
+++ src/test/java/med/vol/api/repository/DoctorRepositoryTest.java
@@ -0,0 +1,18 @@
+package med.vol.api.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class DoctorRepositoryTest {
+
+    @Test
+    void getRandomDoctor() {
+    }
+}
